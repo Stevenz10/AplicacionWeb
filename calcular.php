13a4aaa5def52ac5b879a7214a7a77321332efc7
Steven Zheng <steven.zheng@utp.ac.pa>
2022-06-30 11:53:16 -0500

    Se agrego el switch y se refactorizo el codigo

--- calcular.php
+++ calcular.php
@@ -3,21 +3,22 @@ $cantidad = $_POST['cantidad'];
 $subtotal = $cantidad*125;
 $numero_cajas = ceil($cantidad/4);
 $flete = $numero_cajas*50;
-if ($cantidad>1000)
+if ($cantidad>1000){
     $descuento=$subtotal*0.15;
+}
 else
 {
-    if($cantidad>100)
+    if($cantidad>100){
         $descuento=$subtotal*0.05;
-    else
+    }
+    else{
         $descuento=0;
+    }
 }
 $total=$subtotal+$flete-$descuento;
 echo "El precio total a pagar es: ".$total."<br>";
 echo "El monto por flete es: ".$flete."<br>";
 echo "El descuento aplicado fue: ".$descuento;
-?>
-
 $edad = 24;
 switch($edad)
 {
@@ -31,3 +32,5 @@ switch($edad)
         echo "El doble de tu edad";
         break;
 }
+
+?>
